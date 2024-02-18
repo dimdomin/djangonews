03aeda01adb1eac9f946a5d37b472f957a987968
dimdomin <145801131+dimdomin@users.noreply.github.com>
2024-02-18 17:18:27 +0300

    Delete Newsport directory

--- Newsport/news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Post, Author, Comment, PostCategory
-
-
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(Author)
-admin.site.register(Comment)
-admin.site.register(PostCategory)
